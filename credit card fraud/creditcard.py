6d7d044d34702fb311d067c16135de43407e352e
Cvamprogramming <42740629+Cvamprogramming@users.noreply.github.com>
2019-07-07 20:18:37 +0530

    Update creditcard.py

--- credit card fraud/creditcard.py
+++ credit card fraud/creditcard.py
@@ -1,22 +1,67 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  5 15:07:23 2019
 
-@author: KIIT
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+from sklearn.pipeline import make_pipeline
+from imblearn.pipeline  import make_pipeline as make_pipeline_imb
+from imblearn.over_sampling import SMOTE
 
+# data preprocessing
 dataset = pd.read_csv('creditcard.csv')
- 
-X = dataset.iloc[:,:-1].values
-y= dataset.iloc[:, -1].values;
+X = dataset.iloc[ : , :-1].values
+y= dataset.iloc[:,-1].values
+#Visulaizinig the dataset 
+fig, ax = plt.subplots(1,1)
+ax.pie(dataset.Class.value_counts(),explode=(0,0.1), autopct='%1.1f%%', labels = ['Genuine', 'Fraud'], colors=['y','r'])
+plt.axis = 'equal'
 
-#splitting dataset
-from sklearn.cross_validation import train_test_split
-X_train,X_test,y_train,y_test =train_test_split(X,y,test_size=1/3,random_state=0)
+#seperating the data set in two parts 
+data_frame_1 = dataset[dataset['Class'] == 0]
+data_frame_2 = dataset[dataset['Class'] == 1]
+
+#Visualizing frequency distribution 
+for i in range(1,29):
+    sns.distplot(data_frame_1.iloc[:,i])
+    sns.distplot(data_frame_2.iloc[:,i], color='r')
+    plt.show()
+
+'''
+It can be observed that, frequency distribution for both the classes 
+("0" and "1") against features V8, V13, V15, V20, V21, V22, V23, V24,
+V25, V26, V27 and V28 are approximately similar. These features would
+most certainly not help out for the purpose of differentiating between
+class 0 and 1.So, we drop these features from our data_frames so as
+to make our model less complex.
+'''
+data_frame_1 = data_frame_1.drop(columns=["V8","V13","V15","V20","V21","V22","V23","V24","V25","V26","V27","V28"])
+data_frame_2 = data_frame_2.drop(columns=["V8","V13","V15","V20","V21","V22","V23","V24","V25","V26","V27","V28"])
+
+
+for_count_0 = data_frame_1.head(2500)
+for_count_1 = data_frame_2.head(50)
+
+count_0_downward = data_frame_1.tail(len(data_frame_1)-2500)
+count_1_downward = data_frame_2.tail(len(data_frame_2)-50)
+
+
+
+X_train = pd.concat([count_0_downward,count_1_downward])
+X_train_set = X_train.drop(columns=['Class']).values
+y_train = X_train['Class'].values
+
+X_test = pd.concat([for_count_0,for_count_1])
+X_test_set = X_test.drop(columns=['Class']).values
+y_test = X_test['Class'].values
+
+#Synthetic Minority Oversampling Technique Algorithm
+sm = SMOTE(random_state=42)
+X_res, y_res = sm.fit_resample(X_train, y_train)
+
+np.count_nonzero(y_res == 0)
+
 
 #scaling the dataset
 from sklearn.preprocessing import StandardScaler
@@ -24,63 +69,23 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
 
-#Applying PCA
-from sklearn.decomposition import PCA
-pca = PCA(n_components = 2)
-X_train = pca.fit_transform(X_train)
-X_test = pca.fit_transform(X_test)
-explained = pca.explained_variance_ratio_
-
-
+    
 #applying naive bayes algorithm
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
-classifier.fit(X_train,y_train)
+classifier.fit(X_res,y_res)
 
 #prediction 
 y_pred = classifier.predict(X_test)
+
 
 #checking the accuracy
 from sklearn.metrics import  confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
 
+
 #Applying Kfold cross validation
 from sklearn.model_selection import cross_val_score
 accuries = cross_val_score(estimator=classifier,X=X_train,y = y_train,cv =10)
 
-#Graphically on train set
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Test set)')
-plt.xlabel('val1')
-plt.ylabel('val2')
-plt.legend()
-plt.show()
-
-#Graphically on Test set
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
 
